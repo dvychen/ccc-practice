78702611db512affe90957c3306a2fe872fa3930
dvychen <dvychen@gmail.com>
2020-02-10 22:53:16 -0500

    Added bones of q4.

---
+++ q4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+// Need list?
+
+int main() {
+  // Input
+  int N;
+  cin >> N;
+  int balls[N];
+  for (int i = 0; i < N; i++) {
+    cin >> balls[i];
+  }
+  // Solution
+
+}
